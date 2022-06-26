d7db71f4341ef9f545f850c3c2e700a2bfd273bf
洪金敏 <hongjinmin.hjm@alibaba-inc.com>
2022-06-26 14:56:01 +0800

    创建空项目

---
+++ Assets/Scripts/Launch.cs
@@ -0,0 +1,22 @@
+/*************************************************************************************
+ *
+ * 文 件 名:   Launch.cs
+ * 描    述: 
+ * 
+ * 创 建 者：  洪金敏 
+ * 创建时间：  2022-06-26 14:20:01
+*************************************************************************************/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Launch : MonoBehaviour
+{
+    [SerializeField]
+    private Image ImgLogo;
+
+    [SerializeField]
+    private GameObject[] ProductModelGroup;
+}
